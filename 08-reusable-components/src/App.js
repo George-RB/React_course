5c0db7d1bf53357f762572a3eb50774e5afd3d07
George Kartavenko <SocialImp@mail.ru>
2024-08-31 13:20:35 +0300

    :add: new components

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MyComponent from './components/MyComponent';
+import NewComponent from './components/NewComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <NewComponent />
+      <NewComponent />
+      <MyComponent />
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 08-reusable-components/src/components/NewComponent.js
@@ -0,0 +1,10 @@
+function NewComponent() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <p>Some text for new component</p>
+    </div>
+  );
+}
+
+export default NewComponent;
