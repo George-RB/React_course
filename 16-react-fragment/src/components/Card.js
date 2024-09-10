0900e620db0e991645683b31aaf26eaeb6807803
George Kartavenko <SocialImp@mail.ru>
2024-09-10 12:42:59 +0300

    :feat: fragment react

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    // <React.Fragment>
+    //   <h1>George</h1>
+    //   <h3>I am recording React course</h3>
+    //   <button>Like</button>
+    // </React.Fragment>
+    <>
+      <h1>George</h1>
+      <h3>I am recording React course</h3>
+      <button>Like</button>
+    </>
+  );
+};
+
+export default Card;
