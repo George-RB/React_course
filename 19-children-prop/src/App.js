39ae98847aad1abc8b6fab6cae0dd4f16db0b821
George Kartavenko <SocialImp@mail.ru>
2024-09-11 22:02:32 +0300

    :add: children

---
+++ 19-children-prop/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Wrapper from './components/Wrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <h2>Some text from Wrapper</h2>
+        <button>Click</button>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
