af36523715d101c72fd4e761340a1a979ca5ee2a
George Kartavenko <SocialImp@mail.ru>
2024-09-11 21:38:33 +0300

    :add: controlled input

---
+++ 18-controlled-input/src/components/Login.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleFormSubmit(event) {
+    return event.preventDefault();
+
+    const userData = {
+      username,
+      password,
+    };
+    console.log(userData);
+    alert(JSON.stringify(userData));
+  }
+  return (
+    <>
+      <h1>Login form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:{' '}
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password:{' '}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
