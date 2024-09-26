fea1c08809e4709220a54c20aabad49cc7f945ab
George Kartavenko <SocialImp@mail.ru>
2024-09-26 20:42:22 +0300

    :add: ver 1

--- 24-todo-app-v1/src/components/Todos/Todo.js
+++ 24-todo-app-v1/src/components/Todos/Todo.js
@@ -1,3 +1,4 @@
+import { RiTodoFill } from 'react-icons/ri';
 import styles from './Todo.module.css';
 
 function Todo({ todo, index, deleteTodo }) {
@@ -8,6 +9,7 @@ function Todo({ todo, index, deleteTodo }) {
         deleteTodo(index);
       }}
     >
+      <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo}</div>
     </div>
   );
